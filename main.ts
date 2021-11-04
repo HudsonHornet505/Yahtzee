eac918e29160377b6f864bd39a21fefc011dcbf0
HudsonHornet505 <92798362+HudsonHornet505@users.noreply.github.com>
2021-11-04 09:34:25 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,34 @@
+let Dice1R2 = 0
+let Dice1 = 0
+let Dice2 = 0
+let Dice3 = 0
+let Dice4 = 0
+let Dice5 = 0
+input.onPinPressed(TouchPin.P0, function () {
+	
+})
+input.onButtonPressed(Button.A, function () {
+    Dice1R2 = Dice1
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+input.onPinPressed(TouchPin.P1, function () {
+	
+})
+input.onGesture(Gesture.Shake, function () {
+    Dice1 = randint(1, 6)
+    basic.showNumber(Dice1)
+    basic.pause(500)
+    Dice2 = randint(1, 6)
+    basic.pause(500)
+    basic.showNumber(Dice1)
+    Dice3 = randint(1, 6)
+    basic.pause(500)
+    basic.showNumber(Dice1)
+    Dice4 = randint(1, 6)
+    basic.pause(500)
+    basic.showNumber(Dice1)
+    Dice5 = randint(1, 6)
+    basic.pause(500)
+})
